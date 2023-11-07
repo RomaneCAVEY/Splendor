37a57bac0ac21ec1a9b0eaaf706a230f7c9deb64
Constantin Palluat <constantin.palluat@enseirb-matmeca.fr>
2023-11-07 18:21:38 +0100

    dernier push

---
+++ src/builder.c
@@ -0,0 +1,10 @@
+#include "builder.h"
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+struct builder_t* builder_in_game[MAX_BUILDERS];
+
+void init_builders(unsigned int seed){
+}
--- src/structure.c
+++ src/structure.c
@@ -21,8 +21,21 @@ struct token_t create_complex_token(unsigned int c[NUM_COLORS]){
 struct builder{
     char level;
     int points;
-    enum color_t ressource;
-    enum color_t production;
+    struct token_t ressource;
+    struct token_t production;
 
 
 };
+
+struct market{
+    int nbr_token;
+    struct token_t token_in_market; /*Le market est du type [0,1,3,...,NUM_COLORS] avec chaque position correspondant à la couleur d'un jeton*/
+
+};
+
+struct guilde{
+    int nbr_builder;
+    struct builder builder_in_guilde[MAX_BUILDERS];
+
+};
+
