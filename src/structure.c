4959e2b306169530e84d44bc8407af2549256bcd
Romane Cavey <romane.cavey@enseirb-matmeca.fr>
2023-11-07 15:19:00 +0100

    depot structure

---
+++ src/structure.c
@@ -0,0 +1,24 @@
+#include "token.h"
+#include "builder.h"
+
+struct token_t create_simple_token(enum color_t c){
+    struct token_t token;
+    token.c[0]=c;
+    return token;
+
+};
+
+
+struct token_t create_complex_token(unsigned int c[NUM_COLORS]){
+    struct token_t token;
+    token.c= c;
+};
+
+struct builder{
+    int level;
+    int points;
+    enum color_t ressource;
+    enum color_t production;
+
+
+};
