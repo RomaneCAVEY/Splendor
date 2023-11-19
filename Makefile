adaa5f41ca365625f34e520dee345b6cd3c4ccef
Romane Cavey <romane.cavey@enseirb-matmeca.fr>
2023-11-19 22:06:52 +0100

    loop with gtop()

--- Makefile
+++ Makefile
@@ -2,15 +2,13 @@ NUM_COLORS ?= 5
 NUM_LEVELS ?= 2
 NUM_TOKENS ?= 25
 MUNIFICENCE_FLAGS = -DNUM_COLORS=$(NUM_COLORS) -DNUM_LEVELS=$(NUM_LEVELS) -DNUM_TOKENS=$(NUM_TOKENS)
-CFLAGS = -Wall -Wextra -std=c99 -g3 $(MUNIFICENCE_FLAGS) -Isrc -Itst
+CFLAGS = -Wall -Wextra -std=c99 -g3 $(MUNIFICENCE_FLAGS)
 BUILD_DIR = ./build
 SRC_DIR = ./src
 
 
 all: project
-test: test_token_use test_token
-	./test_token
-	./test_token_use
+test: test
 
 %.o: %.c
 	echo compiling $< into $@
@@ -33,11 +31,16 @@ test_display: tst/test_display.o src/color.o src/token.o src/game.o src/player.o
 token: $src/token.o
 	$(CC) $(CFLAGS) token.o -o token
 
-test_token: tst/test_token.o src/color.o
+test: tst/test_token.o src/color.o
 	$(CC) $(CFLAGS) $^ -o $@
 
 test_builder: tst/test_builder.o src/builder.o src/token.o src/color.o
 	$(CC) $(CFLAGS) $^ -o $@
+
+
+clang_custom_lib_support:
+	(echo $(INC_FLAGS) | sed 's/ /\n/g') > compile_flags.txt
+
 
 
 clean:
--- src/project.c
+++ src/project.c
@@ -18,34 +18,64 @@
 
 #include <threads.h>
 #include <time.h>
-
+#include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 
-#define SEED 0
-#define MAX_TURN 40
-#define NB_MIN_PARAM 1
 #define NB_PLAYERS 2
+
 
 struct player players[NB_PLAYERS];
 
-int main(int argc, char * argv[]) {
-    int max_turn= atoi(argv[1]);
-    srand(SEED);
-    if (argc < NB_MIN_PARAM) {
-        printf("there is a mistake, too few arguments");
-        return EXIT_FAILURE;
-    }
+int main(int argc, char *argv[]){
+           int max_turn;
+           int seed;
+           int seed_builder;
+           int opt;
+
+//Mistake in this part: 
+           max_turn = 0;
+           seed = 0;
+           seed_builder = 0;
+           while ((opt= getopt(argc, argv, "s:m:c:"))!=(-1)) {
+               switch (opt) {
+               case 's':
+                   seed= atoi(optarg);
+                   break;
+               case 'm':
+                   max_turn = atoi(optarg);
+                   break;
+                case 'c':
+                   seed_builder = atoi(optarg);
+                   break;
+                   
+               default: /* '?' */
+                   fprintf(stderr, "Usage: %s [-s seed] [-m max_turn]  [-c seed_builder] \n",
+                           argv[0]);
+                   exit(EXIT_FAILURE);
+               }
+           }
+
+           printf("max_turn=%d; seed=%d; seed_builder=%d \n",
+                  max_turn, seed, seed_builder );
+
+           if (optind < argc) {
+               fprintf(stderr, "Expected argument after options \n");
+               exit(EXIT_FAILURE);
+           }
+
+    srand(seed);
 
     //Init player
-
     for (int i = 0; i < NB_PLAYERS; i++) {
         players[i] = init_player();
         players[i].index = i;
     }
     int current_player = (get_random_player(NB_PLAYERS));
+    
     //Init market and guild    
     init_all_tokens(); //comment initialiser les tokens
-    init_builders(0); // Use seed 0 at the beginning of a game   // Same thing
+    init_builders(seed_builder); // Use seed 0 at the beginning of a game   // Same thing
     init_guild(); // il faut initialiser les guild;
     init_market();
     int nb_turns_not_played = 0;
@@ -53,6 +83,7 @@ int main(int argc, char * argv[]) {
 
     while (!(has_won(players) && (nb_turns_not_played < 2)) && nb_turns < max_turn) {
         printf("This is the turn %d \n", nb_turns);
+        printf("this is the points %d of the current player, player %d\n", players[current_player].points,current_player);
         int index;
         int possibility_to_pay=0;
         for (int i = 0; i < guild_nbr_builder(); i++) {
@@ -75,7 +106,7 @@ int main(int argc, char * argv[]) {
         if (possibility_to_pay) {
             //put in the market the tokens which were useful to pay the builder, except if they are builders
             pay(players,index, current_player);
-            printf("this is the points %d of player %d\n", current_player, players[current_player].points);
+            printf("this is the points %d of player %d\n", players[current_player].points,current_player);
              }
              
         //Else we pick some tokens if it's possible;
@@ -108,7 +139,7 @@ int main(int argc, char * argv[]) {
         nb_turns += 1;
     }
     if (players[0].points >= VICTORY_POINTS) {
-        printf("Victoire du joueur 1 avec %d points", players[0].points);
+        printf("Victoire du joueur 0 avec %d points", players[0].points);
         return 1;
     }
     if (nb_turns_not_played >= 2 || (nb_turns==max_turn && players[0].points==players[1].points )) {
@@ -116,6 +147,6 @@ int main(int argc, char * argv[]) {
         return 1;
     }
     
-    printf("Victoire du joueur 2 avec %d points", players[1].points);
+    printf("Victoire du joueur 1 avec %d points", players[1].points);
     return 0;
 }
