308bffa5bb8b9e42818aba240fe9d6882c156012
Romane Cavey <romane.cavey@enseirb-matmeca.fr>
2023-11-24 17:25:14 +0100

    depot achiev1

---
+++ achiev1/achiev1_stack.c
@@ -0,0 +1,12 @@
+#include "game.h"
+#include "second_token.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+struct guild guild;
+struct market market;
+
+
---
+++ achiev1/tst/test.c
@@ -0,0 +1,85 @@
+#include "builder.h"
+
+#include "token.h"
+
+#include "color.h"
+
+#include "player.h"
+
+#include "game.h"
+
+#include "second_token.h"
+
+#include "second_builder.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+
+
+int main(int argc, char* argv[])
+{   
+    if (argc< 1){
+        printf("too few arguments");
+    }
+    else {
+    printf("arguments took: %s", argv[1]);
+    }
+    printf("Voici les tests: \n \n ******************** \n");
+    if(!test_builder()){
+        printf("\n \n ==========================================");
+        printf("test builder : ");
+        printf("test passed");
+    }
+    else {printf("test failed");
+    }
+    printf("\n \n ========================================== \n \n");
+    if(!test_main()){
+        printf("test main: \n");
+        printf("test passed");
+    }
+    else {printf("test failed");
+    }
+    printf("\n \n ========================================== \n \n");
+    
+    if(!test_init()){
+        printf("test init() \n");
+        
+        printf("test passed");
+        }
+    else {printf("test failed");
+    }
+    printf("\n \n ========================================== \n \n");
+    if(!test_display()){
+        printf("test display \n");
+        printf("test passed");
+        }
+    else {printf("test failed");
+    }
+    printf("\n \n ========================================== \n \n");
+    if(!token_use()){
+        printf("test token use \n");
+        printf("test passed");
+        }
+    else {printf("test failed");
+    }
+    printf("\n \n ========================================== \n \n");
+    if(!test_token_use()){
+        printf("test_token_use \n");
+        printf("test passed");
+        }
+    else {printf("test failed");
+    }
+    return 0;
+}
+
+
+
+
+
+
+
